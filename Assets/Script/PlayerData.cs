c5cd4789e7322d8c5fa1fd5405d466724c50893a
Takahashi <megane.nt5mf@gmail.com>
2022-01-02 23:47:17 +0900

    プレイヤーデータ・各種設定保持の準備

---
+++ Assets/Script/PlayerData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    // 音ゲパート関係設定
+    public static float HiSpeed;
+
+    // プレイヤー情報
+    public static string PlayerName;
+}
