bfff73f096d2f95f7f9a48ad9f0c531d366fba12
Takahashi <megane.nt5mf@gmail.com>
2022-01-06 16:37:33 +0900

    FansyScrollView導入・仮データで楽曲一覧表示(無限スクロール)機能実装

---
+++ Assets/Script/Cell.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * FancyScrollView (https://github.com/setchi/FancyScrollView)
+ * Copyright (c) 2020 setchi
+ * Licensed under MIT (https://github.com/setchi/FancyScrollView/blob/master/LICENSE)
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FancyScrollView.Example01
+{
+    class Cell : FancyCell<ItemData>
+    {
+        [SerializeField] Text message = default;
+
+        public override void UpdateContent(ItemData itemData)
+        {
+            message.text = itemData.Title;
+        }
+
+        public override void UpdatePosition(float position)
+        {
+            // スクロールの挙動
+            var pos = transform.localPosition;
+            pos.y = Mathf.Lerp( 100, -1100, position );
+            transform.localPosition = pos;
+        
+        }
+
+    }
+}
---
+++ Assets/Script/ScrollView.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * FancyScrollView (https://github.com/setchi/FancyScrollView)
+ * Copyright (c) 2020 setchi
+ * Licensed under MIT (https://github.com/setchi/FancyScrollView/blob/master/LICENSE)
+ */
+
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FancyScrollView.Example01
+{
+    class ScrollView : FancyScrollView<ItemData>
+    {
+        [SerializeField] Scroller scroller = default;
+        [SerializeField] GameObject cellPrefab = default;
+
+        protected override GameObject CellPrefab => cellPrefab;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            scroller.OnValueChanged(UpdatePosition);
+        }
+
+        public void UpdateData(IList<ItemData> items)
+        {
+            UpdateContents(items);
+            scroller.SetTotalCount(items.Count); // データ数セット
+        }
+
+        private void Awake() // Startより前
+        {
+            // 表示用データを格納
+            var datas = Enumerable.Range(1, 10)
+                .Select(i => new ItemData($"Music {i}"))
+                .ToList();
+
+            this.UpdateData(datas);
+        }
+    }
+}
