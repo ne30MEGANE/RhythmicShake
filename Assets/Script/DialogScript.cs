53cf85ade6946706107c12085596b273116a676e
Takahashi <megane.nt5mf@gmail.com>
2022-03-14 16:49:12 +0900

    ハイスピ変更機能を実装

--- Assets/Script/DialogScript.cs
+++ Assets/Script/DialogScript.cs
@@ -1,21 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogScript : MonoBehaviour
 {
     // データ
     float highSpeed;
     string playerName;
+
+    // UI
+    public Slider slider;
+    public Text slider_dsp;
     
     void Start()
     {
         highSpeed = PlayerData.HiSpeed;
         playerName = PlayerData.PlayerName;
+
+        slider.value = highSpeed;
+        slider_dsp.text = "現在の設定 ： " + highSpeed.ToString();
+
     }
 
     public void CloseDialog(){
+        PlayerData.HiSpeed = highSpeed;
+        PlayerData.PlayerName = playerName;
         Destroy(this.gameObject);
+    }
+
+    public void HighSpeedSet(){
+        highSpeed = slider.value;
+        slider_dsp.text = "現在の設定 ： " + highSpeed.ToString();
     }
 
 }
